ab427fa841ee95419cf9cc3dbc4b3edb7ce44b8c
Mário Allan Lehmkuhl de Abreu <marioallan.la@gmail.com>
2019-07-01 12:28:57 -0300

    Update config.py

--- server/config.py
+++ server/config.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-#image_path = '/tmp/images/'
-image_path = '/home/mario/allan_eng/pji3/local/server/images/'
+image_path = '/tmp/images/'
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
